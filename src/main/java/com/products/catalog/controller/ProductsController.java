9b873d5ed492c3512aed3af6b66df08c928fd898
patounder <pmquirozvargas@gmail.com>
2021-12-21 10:46:26 -0300

    add create product service

--- src/main/java/com/products/catalog/controller/ProductsController.java
+++ src/main/java/com/products/catalog/controller/ProductsController.java
@@ -3,10 +3,7 @@ package com.products.catalog.controller;
 import com.products.catalog.model.Product;
 import com.products.catalog.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +26,10 @@ public class ProductsController {
     @GetMapping(value = "/{sku}")
     public Product findById(@PathVariable("sku") String sku){
         return this.productService.findBySku(sku).get();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return this.productService.create(product);
     }
 }
